fbd8ed1159af2040d9a409bc92d5d31e7028b8b0
Saurabh Verma <saurabh1088@gmail.com>
2025-01-14 21:07:16 +0530

    Create networking framework project to be used across all swift projects.

---
+++ SwiftNetworking/SwiftNetworking/SwiftNetworking.h
@@ -0,0 +1,18 @@
+//
+//  SwiftNetworking.h
+//  SwiftNetworking
+//
+//  Created by Saurabh Verma on 14/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftNetworking.
+FOUNDATION_EXPORT double SwiftNetworkingVersionNumber;
+
+//! Project version string for SwiftNetworking.
+FOUNDATION_EXPORT const unsigned char SwiftNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNetworking/PublicHeader.h>
+
+
---
+++ SwiftNetworking/SwiftNetworkingTests/SwiftNetworkingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkingTests.swift
+//  SwiftNetworkingTests
+//
+//  Created by Saurabh Verma on 14/01/25.
+//
+
+import Testing
+@testable import SwiftNetworking
+
+struct SwiftNetworkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
