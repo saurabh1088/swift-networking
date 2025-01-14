286ff59d3faf3e562c7396defea50ed79831e758
Saurabh Verma <saurabh1088@gmail.com>
2025-01-14 21:20:19 +0530

    Added package for networking to be used across all swift projects.

---
+++ Sources/SwiftNetworking/APIRequest.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  SwiftNetworking
+//
+//  Created by Saurabh Verma on 14/01/25.
+//
+
+import Foundation
+
+protocol APIRequest {
+    var baseURL: String { get }
+    var path: String { get }
+    var params: [String: Any]? { get }
+    var httpMethod: HTTPMethods { get }
+}
+
+extension APIRequest {
+    var urlRequest: URLRequest {
+        guard let url = self.url else { fatalError("Error") }
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        return request
+    }
+    
+    var url: URL? {
+        var urlComponents = URLComponents(string: baseURL)
+        urlComponents?.path = path
+        
+        if httpMethod == .GET {
+            guard let queryParams = params as? [String: String] else {
+                fatalError("Error")
+            }
+            urlComponents?.queryItems = queryParams.map({ URLQueryItem(name: $0.key, value: $0.value) })
+        }
+        
+        return urlComponents?.url
+    }
+}
